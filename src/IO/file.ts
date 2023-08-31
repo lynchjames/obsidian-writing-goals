4d8d7dff0c29e66d46ad8ab508538724df33898b
James Lynch <lynchjames@gmail.com>
2023-09-01 00:06:16 +0100

    Removes cache for now due to parsing errors

--- src/IO/file.ts
+++ src/IO/file.ts
@@ -19,13 +19,11 @@ export class WritingGoalsFile {
     }
   
     async loadJson<T>(path: string): Promise<T> {
-      const data = this.fileInMemoryCache ?? await this.loadFile(path);
-      return JSON.parse(data) as T;
+      return JSON.parse(await this.loadFile(path)) as T;
     }
   
     async saveJson<T>(path: string, data: T): Promise<void> {
       const dataToSave = JSON.stringify(data);
-      this.fileInMemoryCache = dataToSave;
       await this.app.vault.adapter.write(path, dataToSave);
     }
 
