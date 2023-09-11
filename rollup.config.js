ec1462de92a70c11c897702d0f73d04d8ecd6614
James Lynch <lynchjames@gmail.com>
2023-09-11 20:36:51 +0100

    Traspiles main.js in root folder

--- rollup.config.js
+++ rollup.config.js
@@ -10,7 +10,7 @@ const TEST_VAULT = 'test-vault/.obsidian/plugins/writing-goals';
 export default {
   input: 'src/main.ts',
   output: {
-    dir: 'dist/',
+    dir: './',
     sourcemap: 'inline',
     format: 'cjs',
     exports: 'default'
@@ -26,7 +26,7 @@ export default {
     typescript(),
     copy({
       targets: [
-        { src: 'dist/main.js', dest: TEST_VAULT },
+        { src: 'main.js', dest: TEST_VAULT },
         { src: ['manifest.json', 'styles.css'], dest: TEST_VAULT }
       ], flatten: true
     })
