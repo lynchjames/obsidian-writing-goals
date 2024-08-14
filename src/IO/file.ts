0eed12cd3f63b2201a1388f935359edb3b4def0b
James Lynch <lynchjames@gmail.com>
2024-08-14 23:28:33 +0100

    Adds path normalisation re #56

--- src/IO/file.ts
+++ src/IO/file.ts
@@ -1,5 +1,5 @@
 import type { App } from "obsidian";
-import { Notice } from "obsidian";
+import { Notice, normalizePath } from "obsidian";
 
 export class WritingGoalsFile {
   app: App;
@@ -10,15 +10,16 @@ export class WritingGoalsFile {
   }
 
   async exists(path: string): Promise<boolean> {
-    return await this.app.vault.adapter.exists(path);
+    return await this.app.vault.adapter.exists(normalizePath(path));
   }
 
   async loadFile(path: string): Promise<string> {
+
     if (!(await this.exists(path))) {
       throw Error(`The file is not found: ${path}`);
     }
     try {
-      return await this.app.vault.adapter.read(path);
+      return await this.app.vault.adapter.read(normalizePath(path));
     } catch (error) {
       const errorMsg = "Writing Goals: error reading file - file may be in use by another process";
       console.log(errorMsg);
@@ -28,7 +29,7 @@ export class WritingGoalsFile {
 
   async loadJson<T>(path: string): Promise<T> {
     try {
-      return JSON.parse(await this.loadFile(path)) as T;
+      return JSON.parse(await this.loadFile(normalizePath(path))) as T;
     } catch (error) {
       const errorMsg = "Writing Goals: error reading JSON file at " + path;
       console.log(errorMsg);
@@ -40,7 +41,7 @@ export class WritingGoalsFile {
   async saveJson<T>(path: string, data: T): Promise<void> {
     const dataToSave = JSON.stringify(data, null, 2);
     try {
-      await this.app.vault.adapter.write(path, dataToSave);
+      await this.app.vault.adapter.write(normalizePath(path), dataToSave);
     } catch (error) {
 	const errorMsg = "Writing Goals: error writing to JSON file at " + path;
 	console.log(errorMsg);
@@ -51,12 +52,11 @@ export class WritingGoalsFile {
   
   async saveCsv(path: string, data: string): Promise<void> {
     try {
-      await this.app.vault.adapter.write(path, data);
+      await this.app.vault.adapter.write(normalizePath(path), data);
     } catch (error) {
       const errorMsg = "Writing Goals: error accessing csv file for history export - file may be in use";
       console.log(errorMsg);
       new Notice(errorMsg);
-
     }
   }
 
